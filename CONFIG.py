ad068f94bd2236c5ce2b550342a292143906e1f3
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-01-02 01:31:53 +0300

    Add server, gateway, and sensor configurations

---
+++ CONFIG.py
@@ -0,0 +1,13 @@
+# Server configuration
+WEB_SERVER_HOST = 'localhost'
+WEB_SERVER_PORT = 8080
+
+# Gateway configuration
+GATEWAY_SOCKET_HOST = 'localhost'
+GATEWAY_SOCKET_PORT = 8001
+
+# Sensor configuration
+TEMPERATURE_SENSOR_HOST = 'localhost'
+TEMPERATURE_SENSOR_PORT = 8002
+HUMIDITY_SENSOR_HOST = 'localhost'
+HUMIDITY_SENSOR_PORT = 8003
