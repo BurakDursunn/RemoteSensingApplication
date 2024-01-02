c64146e67f9edd4b9f7dc12353d42374d3519b4d
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-01-02 16:45:55 +0300

    Refactor code to improve performance and readability

--- gateway.py
+++ gateway.py
@@ -1,187 +1,107 @@
-# gateway.py
 import socket
 import threading
 import time
-import sys
-import http.client
-from CONFIG import GATEWAY_SOCKET_HOST, GATEWAY_SOCKET_PORT, TEMPERATURE_SENSOR_HOST, TEMPERATURE_SENSOR_PORT, HUMIDITY_SENSOR_HOST, HUMIDITY_SENSOR_PORT
+import select
+from CONFIG import TEMPERATURE_SENSOR_HOST, TEMPERATURE_SENSOR_PORT, HUMIDITY_SENSOR_HOST, HUMIDITY_SENSOR_PORT, SERVER_HOST, SERVER_PORT
 
-temperature_lock = threading.Lock()
-humidity_lock = threading.Lock()
+TEMPERATURE_SENSOR_OFF_INTERVAL = 3
+HUMIDITY_SENSOR_OFF_INTERVAL = 7
+CONNECTION_TIMEOUT = 15
 
 
-class TemperatureSensorReciever(threading.Thread):
-    def __init__(self, temperature_sensor_socket):
-        super().__init__()
-        self.temperature_sensor_socket = temperature_sensor_socket
-        self.last_alive = 0
+def handle_temperature_data(data, server_socket):
+    temperature, timestamp = data.split('|')[1:]
+    message = f'TEMP|{temperature}|{timestamp}'
+    server_socket.sendall(message.encode())
 
-    def send_data_to_gateway(self, temperature, timestamp):
-        # Send the data to server
-        connection = http.client.HTTPConnection(
-            GATEWAY_SOCKET_HOST, GATEWAY_SOCKET_PORT)
-        headers = {'Content-type': 'text/plain'}
-        path = '/temperature'
-        data = f"temperature {temperature} {timestamp}"
 
-        try:
-            connection.request('POST', path, data, headers)
-            response = connection.getresponse()
-            print(
-                f'Temperature data sent successfully. Response: {response.status}, {response.reason}')
-        except Exception as e:
-            print(f'Error while sending temperature data: {e}')
-            connection.close()
-        finally:
-            connection.close()
+def handle_humidity_data(data, server_socket):
+    humidity, timestamp = data.split('|')[1:]
+    message = f'HUMID|{humidity}|{timestamp}'
+    server_socket.sendall(message.encode())
 
-    def parse_data(self, data):
-        # Parse the data
-        data = data.split()
-        temperature = data[1]
-        timestamp = data[2]
-        # str to float
-        temperature = float(temperature)
-        # str to timestamp
-        timestamp = float(timestamp)
 
-        # Send the data to gateway
-        self.send_data_to_gateway(temperature, timestamp)
+def handle_alive_message(server_socket, data):
+    timestamp = data.split('|')[1]
+    message = f'ALIVE|{timestamp}'
+    server_socket.sendall(message.encode())
 
-    def run(self):
-        self.last_alive = 0
+
+def handle_data(data, server_socket):
+    if data.startswith('TEMP'):
+        handle_temperature_data(data, server_socket)
+    elif data.startswith('HUMID'):
+        handle_humidity_data(data, server_socket)
+    elif data.startswith('ALIVE'):
+        handle_alive_message(server_socket, data)
+
+
+def temperature_sensor_listener(server_socket):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_temp:
+        s_temp.bind((TEMPERATURE_SENSOR_HOST, TEMPERATURE_SENSOR_PORT))
+        s_temp.listen()
+        temp_conn, _ = s_temp.accept()
+
+        last_alive_time = time.time()
         while True:
-            data = self.temperature_sensor_socket.recv(1024).decode()
+            data = temp_conn.recv(1024)
+
             if data:
-                self.last_alive = 0
-                with temperature_lock:
-                    self.parse_data(data)
+                last_alive_time = time.time()
+                data = data.decode()
+                handle_data(data, server_socket)
             else:
-                if self.last_alive > 3:
-                    print("TEMP SENSOR OFF")
-                    with temperature_lock:
-                        self.send_data_to_gateway(
-                            "OFF", time.time())
+                if time.time() - last_alive_time >= TEMPERATURE_SENSOR_OFF_INTERVAL:
+                    print("Temperature sensor is off")
                     break
-                self.last_alive += 1
-                time.sleep(1)
+
+            time.sleep(1)
+        temp_conn.close()
 
 
-class HumiditySensorReciever(threading.Thread):
-    def __init__(self, humidity_sensor_socket):
-        super().__init__()
-        self.humidity_sensor_socket = humidity_sensor_socket
-
-    def send_data_to_gateway(self, humidity, timestamp):
-        # Send the data to server
-        connection = http.client.HTTPConnection(
-            GATEWAY_SOCKET_HOST, GATEWAY_SOCKET_PORT)
-        headers = {'Content-type': 'text/plain'}
-        path = '/humidity'
-        data = f"humidity {humidity} {timestamp}"
-
-        try:
-            connection.request('POST', path, data, headers)
-            response = connection.getresponse()
-            print(
-                f'Humidity data sent successfully. Response: {response.status}, {response.reason}')
-        except Exception as e:
-            print(f'Error while sending humidity data: {e}')
-            connection.close()
-        finally:
-            connection.close()
-
-    def parse_data(self, data):
-        # Parse the data
-        data = data.split()
-        humidity = data[1]
-        timestamp = data[2]
-        # str to float
-        humidity = float(humidity)
-        # str to timestamp
-        timestamp = float(timestamp)
-
-        # Send the data to gateway
-        self.send_data_to_gateway(humidity, timestamp)
-
-    def run(self):
+def humidity_sensor_listener(server_socket):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_hum:
+        s_hum.bind((HUMIDITY_SENSOR_HOST, HUMIDITY_SENSOR_PORT))
         while True:
-            try:
-                data, address = self.humidity_sensor_socket.recvfrom(1024)
-                if data:
-                    # if data is ALIVE, print it, else parse it
-                    if data.decode() == "ALIVE":
-                        with humidity_lock:
-                            # Send the data to server
-                            self.send_data_to_gateway(
-                                "ALIVE", time.time())
-                    else:
-                        with humidity_lock:
-                            self.parse_data(data.decode())
-            except socket.timeout:
-                print("HUMIDITY SENSOR OFF")
-                with humidity_lock:
-                    # Send the data to server
-                    self.send_data_to_gateway(
-                        "OFF", time.time())
+            ready_sockets, _, _ = select.select(
+                [s_hum], [], [], HUMIDITY_SENSOR_OFF_INTERVAL)
+
+            if ready_sockets:
+                data, _ = s_hum.recvfrom(1024)
+                data = data.decode()
+                handle_data(data, server_socket)
+            else:
+                print("Humidity sensor is off")
                 break
-
-
-def handshake_tcp(socket):
-    if socket.recv(1024).decode() == 'CONNECT':
-        print("Connection established.")
-        socket.send('OK'.encode())
-        return True
-
-
-def handshake_udp(socket):
-    print("Handshaking UDP connection...")
-    socket.sendto('HELLO UDP SERVER'.encode(),
-                  (HUMIDITY_SENSOR_HOST, HUMIDITY_SENSOR_PORT))
-    if socket.recv(1024).decode() == 'HELLO UDP CLIENT':
-        print("Connection established.")
-        return True
+            time.sleep(1)
+        s_hum.close()
 
 
 if __name__ == "__main__":
-    # Connect to sensors
-    # Create a TCP socket for the temperature sensor
-    temperature_sensor_socket = socket.socket(
-        socket.AF_INET, socket.SOCK_STREAM)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.settimeout(CONNECTION_TIMEOUT)
+        try:
+            server_socket.connect((SERVER_HOST, SERVER_PORT))
 
-    # Connect to the temperature sensor
-    temperature_sensor_socket.connect(
-        (TEMPERATURE_SENSOR_HOST, TEMPERATURE_SENSOR_PORT))
+            # Handshake
+            server_socket.sendall('GATEWAY|HANDSHAKE'.encode())
+            handshake_response = server_socket.recv(1024).decode()
+            print(f"Handshake successful: {handshake_response}")
 
-    # Handshake with the temperature sensor
-    if handshake_tcp(temperature_sensor_socket):
-        temperature_sensor_reciever = TemperatureSensorReciever(
-            temperature_sensor_socket)
+            # Start listening to sensors
+            thread_temperature = threading.Thread(
+                target=temperature_sensor_listener, args=(server_socket,))
+            thread_humidity = threading.Thread(
+                target=humidity_sensor_listener, args=(server_socket,))
 
-    # Create a UDP socket for the humidity sensor
-    humidity_sensor_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    humidity_sensor_socket.settimeout(7)
+            thread_temperature.start()
+            thread_humidity.start()
 
-    # Handshake with the humidity sensor
-    if handshake_udp(humidity_sensor_socket):
-        humidity_sensor_reciever = HumiditySensorReciever(
-            humidity_sensor_socket)
-
-    # Start the threads
-    temperature_sensor_reciever.start()
-    humidity_sensor_reciever.start()
-
-    try:
-        # Wait for the threads to finish
-        temperature_sensor_reciever.join()
-        humidity_sensor_reciever.join()
-    except KeyboardInterrupt:
-        # Close the sockets
-        temperature_sensor_socket.close()
-        humidity_sensor_socket.close()
-        sys.exit(0)
-    finally:
-        # Close the sockets
-        temperature_sensor_socket.close()
-        humidity_sensor_socket.close()
-        sys.exit(0)
+            thread_temperature.join()
+            thread_humidity.join()
+        except KeyboardInterrupt:
+            print("Exiting...")
+            exit(0)
+        except socket.timeout:
+            print("Server is not responding")
+            exit(0)
