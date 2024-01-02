3c2fafe7dbb462c8c07082d310642c8f13604f39
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-01-02 16:45:49 +0300

    Update server and sensor configurations

--- CONFIG.py
+++ CONFIG.py
@@ -1,14 +1,14 @@
-# CONFIG.py
-# Server configuration
+# Web server configuration
 WEB_SERVER_HOST = 'localhost'
 WEB_SERVER_PORT = 8080
 
-# Gateway configuration
-GATEWAY_SOCKET_HOST = 'localhost'
-GATEWAY_SOCKET_PORT = 8001
+# Server configuration
+SERVER_HOST = 'localhost'
+SERVER_PORT = 8000
 
-# Sensor configuration
+
+# Sensor configurations
 TEMPERATURE_SENSOR_HOST = 'localhost'
-TEMPERATURE_SENSOR_PORT = 8002
+TEMPERATURE_SENSOR_PORT = 8001
 HUMIDITY_SENSOR_HOST = 'localhost'
-HUMIDITY_SENSOR_PORT = 8003
+HUMIDITY_SENSOR_PORT = 8002
