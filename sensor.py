817333d6f27d6a7e298a24a5e2fbc868f238a8d7
Haydar Taha Tunç <tahatunc163@gmail.com>
2024-01-04 18:12:31 +0300

    Add logging functionality to sensor.py

--- sensor.py
+++ sensor.py
@@ -7,6 +7,15 @@ from CONFIG import TEMPERATURE_SENSOR_HOST, TEMPERATURE_SENSOR_PORT, HUMIDITY_SE
 server_off = False
 
 
+def timestamp_to_date(timestamp):
+    return time.strftime('%d/%m/%Y %H:%M:%S', time.localtime(timestamp))
+
+
+def log_data_to_file(sensor_type, data, timestamp):
+    with open(f'{sensor_type}_sensor.txt', 'a') as f:
+        f.write(f'{timestamp_to_date(timestamp)} - {sensor_type}: {data}\n')
+
+
 def temperature_sensor():
     global server_off
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -17,6 +26,8 @@ def temperature_sensor():
                 timestamp = time.time()
                 message = f'TEMP|{temperature}|{timestamp}'
                 s.sendall(message.encode())
+                print(f'Sent: {message}')
+                log_data_to_file('temprature', temperature, timestamp)
                 time.sleep(1)
         except socket.error:
             print("Server is off")
@@ -40,18 +51,22 @@ def humidity_sensor():
                     message = f'HUMID|{humidity}|{timestamp}'
                     s.sendto(message.encode(),
                              (HUMIDITY_SENSOR_HOST, HUMIDITY_SENSOR_PORT))
+                    print(f'Sent: {message}')
+                    log_data_to_file('humidity', humidity, timestamp)
                     last_alive = 0
 
                 if last_alive == 2:
                     message = f'ALIVE|{timestamp}'
                     s.sendto(message.encode(),
                              (HUMIDITY_SENSOR_HOST, HUMIDITY_SENSOR_PORT))
+                    print(f'Sent: {message}')
+                    log_data_to_file('humidity', 'ALIVE', timestamp)
                     last_alive = 0
                 else:
                     last_alive += 1
                 time.sleep(1)
         except socket.error:
-            print("Server is off")
+            print("Gateway is off")
             exit(0)
 
 
